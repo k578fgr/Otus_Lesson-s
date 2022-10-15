1323f30d213747c4c261b1deaa1aaba64520e638
k578fgr <gnatko.kirill@mail.ru>
2022-10-15 17:31:23 +0400

    move in dirrectory

---
+++ terraform-2/app/variable.tf
@@ -0,0 +1,13 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+}
+
+variable zone {
+  description = "Zone"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-app-base"
+}
+
