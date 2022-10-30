58c721b0f9e034f54fa707a0f2531bbdec9344ed
k578fgr <gnatko.kirill@mail.ru>
2022-10-30 21:38:04 +0400

    add modules

--- terraform-2/main.tf
+++ terraform-2/main.tf
@@ -1,5 +1,24 @@
 provider "google" {
   version = "~> 2.15"
   project = var.project
-  region = var.region
+  region  = var.region
+}
+
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  app_disk_image  = var.app_disk_image
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  zone            = var.zone
+  db_disk_image   = var.db_disk_image
+}
+
+module "vpc" {
+  source        = "./modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
 }
